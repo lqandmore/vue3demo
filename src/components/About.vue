2c78aaeef7a60e7f4a886677a10a2a23858b5717
Edward Lee <lqandmore@163.com>
2023-02-07 00:07:02 +0800

    引用元素和组件

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-        <h2>I'm About</h2>
+        <h2 ref="title">I'm About</h2>
+        <navbar ref="foo"></navbar>
+        <button @click="getel">点击</button>
     </div>
 </template>
 
 <script setup lang="ts">
+import navbar from './navbar.vue'
+import {ref} from 'vue'
+const title = ref(null)
+const foo = ref(null)
+function getel() {
 
+    console.log(title.value)
+
+    foo.value.sayHello()
+
+
+}
 </script>
 
 <style scoped>
---
+++ src/components/navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+function sayHello() {
+    console.log("hahaha");
+}
+
+defineExpose({
+    sayHello
+})
+</script>
+
+<style scoped>
+
+</style>
