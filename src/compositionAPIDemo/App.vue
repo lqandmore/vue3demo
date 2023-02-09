f01a5b947168989f54b43c7c9cbfd6e2e70fb695
liquan <lqandmore@163.com>
2023-02-09 17:53:54 +0800

    watchEffect的使用

--- src/compositionAPIDemo/App.vue
+++ src/compositionAPIDemo/App.vue
@@ -3,13 +3,22 @@
     <input type="text" v-model="message" />
     <h2>{{ message }}</h2>
     <h2>{{ name }} --{{ age }}</h2>
-    <h2>{{ infoM }}</h2>
+    <h2 ref="info2">{{ infoM }}</h2>
     <button @click="btnclick">点我</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, toRef, toRefs, computed } from 'vue'
+import {
+  ref,
+  reactive,
+  toRef,
+  toRefs,
+  computed,
+  watchEffect,
+  watch,
+  onMounted
+} from 'vue'
 import useDebounce from './hooks/useDebounce'
 const info = reactive({ name: 'coderwhy', age: 18 })
 const { name, age } = toRefs(info)
@@ -17,10 +26,10 @@ const refAge = toRef(info, 'age')
 const message = useDebounce('hello')
 
 const infoM = computed({
-  get: () => {
+  get() {
     return name.value + ' ' + age.value
   },
-  set: (newValue) => {
+  set(newValue) {
     const arr = newValue.split(' ')
     name.value = arr[0]
     age.value = Number(arr[1])
@@ -29,8 +38,27 @@ const infoM = computed({
 
 function btnclick() {
   info.age++
-  console.log(infoM.value)
+  if (info.age > 25) stop()
 }
+
+const info2 = ref(null)
+
+const stop = watchEffect(
+  (onInvalidate) => {
+    const requestTimer = setTimeout(() => {
+      console.log('网络请求')
+    }, 2000)
+    onInvalidate(() => {
+      clearTimeout(requestTimer)
+    })
+    console.log(infoM.value)
+  },
+  {
+    flush: 'post'
+  }
+)
+
+onMounted(() => console.log(info2.value))
 </script>
 
 <style scoped>
