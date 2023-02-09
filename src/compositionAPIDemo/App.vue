dd233c2a7ed0487620582668ece293bffd6dbc1d
liquan <lqandmore@163.com>
2023-02-09 16:49:25 +0800

    toref的使用

--- src/compositionAPIDemo/App.vue
+++ src/compositionAPIDemo/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-
+    <h2>{{ refAge }}</h2>
+    <h2>{{ name }} --{{ age }}</h2>
+    <button @click="btnclick">点我</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, reactive, toRef, toRefs } from 'vue'
+const info = reactive({ name: 'coderwhy', age: 18 })
+const { name, age } = toRefs(info)
+const refAge = toRef(info, 'age')
 
+function btnclick() {
+  info.age++
+}
 </script>
 
 <style scoped>
-
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 // import App from './App.vue'
 // import App from './组件的v-model/App.vue'
-import App from './animate/App.vue'
+import App from './compositionAPIDemo/App.vue'
 createApp(App).mount('#app')
