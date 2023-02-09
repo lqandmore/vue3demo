0436d3b7f263845ac1748612007acf231d248bdd
liquan <lqandmore@163.com>
2023-02-09 17:24:32 +0800

    计算属性的使用

--- src/compositionAPIDemo/App.vue
+++ src/compositionAPIDemo/App.vue
@@ -3,19 +3,33 @@
     <input type="text" v-model="message" />
     <h2>{{ message }}</h2>
     <h2>{{ name }} --{{ age }}</h2>
+    <h2>{{ infoM }}</h2>
     <button @click="btnclick">点我</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, toRef, toRefs } from 'vue'
+import { ref, reactive, toRef, toRefs, computed } from 'vue'
 import useDebounce from './hooks/useDebounce'
 const info = reactive({ name: 'coderwhy', age: 18 })
 const { name, age } = toRefs(info)
 const refAge = toRef(info, 'age')
 const message = useDebounce('hello')
+
+const infoM = computed({
+  get: () => {
+    return name.value + ' ' + age.value
+  },
+  set: (newValue) => {
+    const arr = newValue.split(' ')
+    name.value = arr[0]
+    age.value = Number(arr[1])
+  }
+})
+
 function btnclick() {
   info.age++
+  console.log(infoM.value)
 }
 </script>
 
